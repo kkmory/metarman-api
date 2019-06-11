b58d8f15e0aa578d0fb62b87c3c626a21247accd
Keisuke Mori <rjfs29kei@gmail.com>
2019-06-11 10:28:28 +0900

    impl basic success case

--- main.rb
+++ main.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 require 'metarman'
 
-get '/' do
-  # Edit this line
+get '/hc' do
+  JSON.pretty_generate({body: 'server working!!', error: nil})
+end
+
+get '/*'  do |icao|
+  err = []
+
+  unless /[A-Z,a-z]{4}/ === icao
+    err.push("invalid parameter. please enter correct ICAO airport code.")
+  end
+
+  data = Metarman.get_with_info(icao)
+  if data == nil
+    err.push("something went wrong")
+  end
+
+  # gem側でエラー投げてあげたほうがよさそう
+
+  if err.nil?
+    JSON.pretty_generate({body: nil, error: err.first})
+  else
+    JSON.pretty_generate({body: data, error: nil})
+  end
 end
